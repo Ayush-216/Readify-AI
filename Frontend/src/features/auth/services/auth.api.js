5e5882ec0ad35dd035e6390a703675b966d9ed65
Ayush Pandey <exploretheuniverse216@gmail.com>
2026-09-01 03:57:29 +0530

    Creating link from frontend to backend api calls

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+//reusable axios instance for making API calls to the backend
+const api = axios.create({
+    baseURL: "http://localhost:3000/api",
+    withCredentials: true
+})
+
+//how api call works for login from frontend to backend
+export async function register({username, email, password}) {
+
+    try{
+      const response = await api.post("/api/auth/register", {
+        username,
+        email,
+        password
+     })
+
+        return response.data
+
+    }catch (error){
+        console.log(error)
+    }
+
+}
+
+export async function login({email, password}) {
+
+    try{
+      const response = await api.post("/api/auth/login", {
+        email,
+        password
+     })
+        return response.data
+
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export async function logout() {
+
+    try{
+      const response = await api.get("/api/auth/logout")
+        return response.data
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export async function getMe() {
+    try{
+      const response = await api.get("/api/auth/get-me")
+        return response.data
+    }catch (error){
+        console.log(error)
+    }
+}
