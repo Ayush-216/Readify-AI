7a73df4a36e01fadbf8976a9b479bdf50e8ae74d
Ayush Pandey <exploretheuniverse216@gmail.com>
2026-09-01 03:17:46 +0530

    Implemented baseline for Frontend

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {useNavigate, Link} from "react-router"
+
+const Register = () => {
+
+    const navigate = useNavigate();
+  
+     const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    }
+  
+  
+    return (
+    <main>
+    <div className="form-container">
+        <h1>Register </h1>
+        <form>
+
+            <form onSubmit={handleSubmit}/>
+
+            <div className="input-group">
+                <label htmlFor="email">Username</label>
+                <input type="username" id="username" name="email" placeholder="Enter username" required />
+            </div>
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter Email address" required />
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter password" required />
+            </div>
+            <button className='button primary-button' >Register</button>
+
+        </form>
+
+        <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+    </div>
+    </main>
+  )
+}
+
+export default Register
